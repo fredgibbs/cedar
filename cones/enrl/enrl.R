4e1868479d5d7ddf76aaf8e057081ac442f6f633
Fred Gibbs <fwgibbs@gmail.com>
2025-02-28 20:04:49 -0700

    more generalized aggregating function to simplify

--- cones/enrl/enrl.R
+++ cones/enrl/enrl.R
@@ -83,6 +83,164 @@ calc_cl_enrls <- function(students,reg_status=NULL) {
 
 
 
+# CROSSLIST FILTER
+xlist_filter <- function(df,action) {
+  message("Welcome to xlist.R!")
+  
+  if (action == "exclude") {
+    print("excluding cross-listed courses (by XL_CRN = 0)...")
+    df <- df %>% filter(XL_CRN == "0")
+    return(df)
+  }
+  
+  
+  # home will filter out all xled rows of a course except the one that matches dept filtering
+  # best used when looking at a single dept's courses
+  if (action == "home") {
+    print("filter XL entries to match subj or dept filter")
+    
+    # NEED BETTER WAY OF FILTERING OUT COURSES THAT ORIGINATE FROM OUTSIDE A DEPT
+    
+    # probably best way is to use HR list to filter by appointment.
+    # if not appointed in dept, then it shouldn't show up. 
+    
+    # ONE QUICK WAY IS TO keep only rows that aren't xled or if the xl subject is same as subject filter
+    # this weeds out courses that are xled with only other depts
+    # but it keeps courses xled within the dept, EVEN IF they are also xled outside
+    # DONT DO THIS! Because it eliminates xled courses that aren't xled within "home" dept (most, except for ccs)
+    df <- df %>% filter (XL_SUBJ == "0" | XL_SUBJ == SUBJ)
+    return(df)
+  }
+  
+  
+  
+  # compress will compress all the rows of a xled course into one, using the subj code that has highest enrollment
+  # best used when looking at course lists across departments
+  if (action == "compress") {
+    print("compressing XL entries into single course...")
+    
+    # need to get XL_CODES that aren't 0 and set them aside to better filter XL courses
+    non_xl <- df %>% filter (XL_CODE == "0")
+    xl_only <- df %>% filter (XL_CODE != "0")
+    
+    # group by code and order to get highest enrolled listed first 
+    # we'll consider this the "home" dept even though there isn't such a thing technically.
+    xl_only <- xl_only %>% group_by(XL_CODE) %>% arrange(desc(ENROLLED))
+    
+    # grab non-dupes (paired by term and xl_code, since xl_codes are unique only within each term)
+    xl_only <- xl_only[!duplicated(xl_only[ , c("TERM", "XL_CODE")]), ]  # Apply duplicated
+    
+    # output for inspection
+    #xl_only  %>%  arrange(TERM,desc(total_enrl)) %>% tibble::as_tibble() %>% print(n = nrow(.), width=Inf)
+    
+    # now that i have just one entry for each xl'ed course, combine with all non-xled courses for full list
+    print("combining XLed AND non-XL courses...")
+    output <- rbind(xl_only, non_xl) %>% arrange(TERM,desc(total_enrl))
+    return(output)
+  }
+}
+
+
+compress_aop_pairs <- function (courses,opt) {
+  message("compressing AOP courses into single row...")
+  
+  # for testing...
+  # courses <- load_courses(opt)
+  # opt <- list()
+  # #opt[["course"]] <- "BIOL 2305"
+  # opt[["term"]] <- "202210"
+  # courses <-  filter_DESRs(courses,opt)
+  
+  # for clarity, combine aop and twin courses into single entry
+  # test to see if we're filtering by dept
+  courses <- courses %>%  group_by(TERM, XL_CODE)
+  
+  # get just AOP courses
+  courses_aop <- courses %>% filter (INST_METHOD == "MOPS")
+  
+  # AOP sections don't necessarily have a partner, so remove those without one
+  # TODO: handle case of AOP course having partner, but not being crosslisted
+  # might be able to check on course title
+  courses_aop <- courses_aop %>% filter (XL_CODE != "0")
+  
+  # get pairs of aop and twin section
+  aop_pairs <- courses_aop %>% filter (XL_CODE %in% courses_aop$XL_CODE) %>% 
+    distinct(CRN, .keep_all = TRUE) %>% 
+    group_by(TERM,XL_CODE)
+  
+  # to collapse the aop and online section into one row, get each section's enrollment
+  aop_pairs <- aop_pairs %>% mutate (sect_enrl = ENROLLED, pair_enrl = total_enrl - ENROLLED)
+  
+  # arrange by inst_method, and take first row of group
+  aop_single <- aop_pairs %>% arrange(INST_METHOD) %>% filter (row_number() == 1)
+  # message("aop sections:")
+  # print(aop_single)
+  
+  # since compressing two sections into one, change ENROLLED to mimic total_enrl
+  # otherwise, compressing effectively deletes the non-aop section enrollment
+  aop_single <- aop_single %>% mutate (ENROLLED = total_enrl)
+  
+  # remove all pairs from orig course list
+  courses <- courses %>% filter (!(XL_CODE %in% courses_aop$XL_CODE)) %>% distinct(CRN, .keep_all = TRUE) %>% 
+    group_by(TERM,XL_CODE)
+  
+  # add all single rows
+  courses <- rbind(courses,aop_single)
+  
+  message("returning compressed aop rows...")
+  
+  return(courses)
+} # end compress_aop_pairs
+
+
+merge_hr_data <- function (courses) {
+  
+  ############ merge personnel data with course data
+  message("welcome to merge_hr_data!")
+  
+  # get faculty data to associate title with person in course listings
+  file_name <- paste0(cedar_data_dir,"processed/fac_by_term.Rda")
+  message("loading ",file_name,"...")
+  load(file_name)
+  
+  message("adjusting data types...")
+  courses$`PRIM_INST_ID` <- as.double(courses$`PRIM_INST_ID`)
+  courses$`TERM` <- as.character(courses$`TERM`)
+  
+  # disregard as_of_date in HR data
+  fac_by_term <- fac_by_term %>% select (-c(as_of_date))
+  
+  # merge faculty and course data
+  # merging DEPT fields means an instructor w/o an appt % or other kind of admin appt with that DEPT 
+  # will not have a title listed on the enrl reporting tools
+  message("merging faculty data with course data...")
+  courses <- merge(courses,fac_by_term,by.x=c("TERM","PRIM_INST_ID"),by.y=c("term_code","UNM ID"),all.x=TRUE)
+  courses <- courses %>% select (-c(DEPT.y))
+  courses <- courses %>% rename (DEPT = DEPT.x)
+  
+  message("done merging HR data.")
+  
+  return(courses)
+} # end merge_hr_data
+
+
+# generic summary function based on group_cols
+# replaces the many variants of aggregate
+summarize_courses <- function (courses, group_cols) {
+  message("summarizing courses with group_cols...")
+  
+  # TODO: set default group_cols (like basic subj_crse)
+  if (is.null(group_cols)) {
+    group_cols <- c("TERM","SUBJ","SUBJ_CRSE","level","gen_ed_area")
+  }
+  summary <- courses %>% ungroup() %>% group_by_at(group_cols) %>% 
+    summarize(.groups="keep", sections=n(),avg_size=round(mean(ENROLLED),digits=1),enrolled=sum(ENROLLED),avail=sum(SEATS_AVAIL),waiting=sum(WAIT_COUNT))
+  
+  return(summary)
+}
+
+
+
 
 # basic summary summarizes different sections into a single row
 
@@ -136,6 +294,14 @@ agg_by_course <- function(courses,opt) {
   return(summary)
 }
 
+agg_by_subj_crse <- function(courses,opt) { 
+  message("agg_by_subj_course: summarize all types of course sections into single row based on SUBJ_CRSE (not course title)):")
+  summary <- courses %>% group_by(TERM,SUBJ,SUBJ_CRSE,level,gen_ed_area) %>% 
+    summarize(.groups="keep",sections=n(),avg_size=round(mean(ENROLLED),digits=1),enrolled=sum(ENROLLED),avail=sum(SEATS_AVAIL),waiting=sum(WAIT_COUNT))
+    
+  return(summary)
+}
+
 
 agg_by_course_term <- function(courses,opt) { 
   message("agg_by_course_term:")
@@ -185,25 +351,32 @@ agg_by_college_level <- function(courses,opt) {
 
 
 ############# aggregate function (for enrollment summaries)
-aggregate_courses <- function(courses,opt) {
+aggregate_courses <- function(courses, opt, group_cols) {
 
   agg_by <- opt$aggregate
   
-  if (agg_by == "course") {
+  
+  if (!is.null(group_cols)) {
+    message("group_cols is not null...")
+    summary <- summarize_courses(courses,group_cols)
+  }
+  else if (agg_by == "course") {
     summary <- agg_by_course(courses,opt)
   }
-  if (agg_by == "method") {
+  else if (agg_by == "method") {
     summary <- agg_by_method(courses,opt)
   }
-  
+
   else if (agg_by == "course_type") {
     summary <- agg_by_course_type(courses,opt) 
   }
   else if (agg_by == "course_method") {
     summary <- agg_by_course_method(courses,opt) 
   }
-  
-    else if (agg_by == "course_term") {
+  else if (agg_by == "subj_crse") {
+    summary <- agg_by_subj_crse(courses,opt)
+  }
+  else if (agg_by == "course_term") {
     summary <- agg_by_course_term(courses,opt) 
   }
   else if (agg_by == "dept") {
@@ -294,7 +467,7 @@ get_enrl_for_dept_report <- function(courses, d_params) {
 
 
 ###################################
-get_enrl <- function (courses,opt) {
+get_enrl <- function (courses,opt,group_cols=NULL) {
 
   ########## for studio testing
   # opt <- list()
@@ -311,9 +484,12 @@ get_enrl <- function (courses,opt) {
   }
   
   # filter courses according to options
+  # TODO: in practice, filtering is usually done by the caller so this is redundant
+  # TODO: move aop compression into filter_DESRs -- it's a kind of filtering,
   courses <- filter_DESRs(courses, opt)
 
   # add academic year field
+  # TODO: need to move into load_courses
   courses <- add_acad_year (courses, "TERM")
   
   ### AOP COMPRESSION
@@ -340,9 +516,12 @@ get_enrl <- function (courses,opt) {
   courses <- courses %>% distinct() %>% 
     arrange(TERM,SUBJ_CRSE,CRSE_TITLE,INST_METHOD)
   
-  if(!is.null(opt$aggregate)) {
-    courses <- aggregate_courses(courses,opt)
-  } 
+  # check if aggregating
+  if(!is.null(opt$aggregate) || !is.null(group_cols)) {
+    courses <- aggregate_courses(courses, opt, group_cols)
+  } else {
+    message("no aggregating!")
+  }
 
   message("all done in get_enrl! returning data from get_enrl with ", nrow(courses) ," rows...\n")
   return(courses)
