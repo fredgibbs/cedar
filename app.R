85ffceea5e06d69eadd0f8446ad46d3c189318ae
Fred Gibbs <fwgibbs@gmail.com>
2025-03-01 11:55:04 -0700

    Update app.R

--- app.R
+++ app.R
@@ -22,13 +22,15 @@ load_funcs(cedar_base_dir)
 #load(url("https://www.dropbox.com/scl/fi/8jxi6y5goz44ub5vsbms9/degrees.Rda?rlkey=we9lgiqkw0sq6vjtds6tpr08v&st=ch13dtit&dl=1"))
 #load(url("https://unmm-my.sharepoint.com/:u:/g/personal/fwgibbs_unm_edu/EUsiRM6_Xr9Ng-ygeuWDvNwB75F2tZuHSa_nHSBrepsLnw?download=1"))
 
+load(url("https://unmm-my.sharepoint.com/:u:/g/personal/fwgibbs_unm_edu/EcFs0ZAuy35IgjAQ593NAIwBcnocDZ1u6oLALNgVZRs30g?e=ltXRyh?download=1"))
+
 opt <- list()
 opt[["uel"]] <- TRUE
 opt[["level"]] <- "lower" # for faster testing
 courses <- load_courses(opt=NULL) 
 courses <- courses %>%  filter_DESRs(opt) 
 
-students <- load_students(opt=NULL)
+#students <- load_students(opt=NULL)
 
 
 # Define UI for application that draws a histogram
