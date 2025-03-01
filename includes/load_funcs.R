d5f35a391b12dc5f0028d10e4abd89f0f61322eb
Fred Gibbs <fwgibbs@gmail.com>
2025-02-28 19:58:59 -0700

    new single file for loading includes

---
+++ includes/load_funcs.R
@@ -0,0 +1,29 @@
+load_funcs <- function(cedar_base_dir) {
+  message("loading CEDAR cones...")
+  setwd(cedar_base_dir)
+  source("includes/config.R")
+  
+  source("includes/mappings.R")
+  source("includes/lists.R")
+  source("includes/excluded_courses.R")
+  source("includes/gen_ed_courses.R")
+  source("includes/misc_funcs.R")
+  source("includes/filter_class_list.R")
+  source("includes/filter_DESRs.R")
+  
+  source("cones/enrl/enrl.R")
+  source("cones/forecast/forecast.R")
+  source("cones/forecast-report/forecast-report.R")
+  
+  source("cones/course-report.R")
+  source("cones/dept-report.R")
+  source("cones/headcount.R")
+  source("cones/degrees.R")
+  source("cones/credit-hours.R")
+  source("cones/sfr.R")
+  source("cones/waitlist.R")
+  source("cones/gradebook.R")
+  source("cones/lookout.R")
+  source("cones/regstats.R")
+  source("cones/rollcall.R")
+}
