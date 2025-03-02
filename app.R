bbf95b0964ebac6e25c9de74deeb4ad85f244562
Fred Gibbs <fwgibbs@gmail.com>
2025-03-02 06:36:06 -0700

    Update app.R

--- app.R
+++ app.R
@@ -23,6 +23,7 @@ load_funcs(cedar_base_dir)
 #load(url("https://unmm-my.sharepoint.com/:u:/g/personal/fwgibbs_unm_edu/EUsiRM6_Xr9Ng-ygeuWDvNwB75F2tZuHSa_nHSBrepsLnw?download=1"))
 
 #load(url("https://unmm-my.sharepoint.com/:u:/g/personal/fwgibbs_unm_edu/EcFs0ZAuy35IgjAQ593NAIwBcnocDZ1u6oLALNgVZRs30g?download=1"))
+desrs <- Sys.getenv("desrs") 
 load(url(desrs))
 courses <- completed_and_ongoing_courses
 
