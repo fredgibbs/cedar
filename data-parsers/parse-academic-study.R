dd864e1c62582755db5d66993bfc0516a459169c
Fred Gibbs <fwgibbs@gmail.com>
2025-03-03 07:59:33 -0700

    generalize data parsers

--- data-parsers/parse-academic-study.R
+++ data-parsers/parse-academic-study.R
@@ -1,55 +1,5 @@
-# this script loads downloaded excel file(s) from the academic study guided ad-hoc report 
-# and produces data/processed/academic-study.feather
+parse <- function (new_students) {
 
-pacman::p_load(tidyverse, readxl, fs, data.table, lubridate,feather)
-
-message("Welcome to parse-academic-study!")
-
-# load basic includes 
-source("load-parser-includes.R")
-
-# get list of files in downdloads folder and process one by one
-message("getting list of downloaded files...")
-file_list <- dir_ls(paste0(cedar_data_dir,'downloads/academic-study'))
-
-for (file in file_list) {
-  
-  # uncomment for studio testing and stepping through each file
-  # file <- file_list[1]
-
-  # recognize if rebuilding database by detecting no file
-  # this reloads existing file even if parsing multiple files; not the most efficient but a good check.
-  message("loading previous data...")
-  oldfile <- paste0(cedar_data_dir,"processed/academic-study.feather")
-  if (file.exists(oldfile)) {
-    old_students <- read_feather(paste0(cedar_data_dir,"processed/academic-study.feather"))
-    message("loaded ",nrow(old_students) ," rows.")
-    rebuild <- FALSE
-  }
-  else {
-    message("no previous academic study data found.")
-    old_studdents <- tibble()
-    rebuild <- TRUE
-  }
-  
-  message("loading latest student data...")
-  new_students <- read_xlsx(file, col_types = "text")
-  message("loaded ",nrow(new_students) ," rows. processing...")
-  
-  if (!rebuild) {
-    # get term code from new file and remove that term's data from previous data
-    # assumes there is only one term per file, which is always true unless myreports changes
-    message("removing old data duplicated in new file (by term)...")
-    current_term <- new_students$`Academic Period`[1] 
-    old_students <- old_students %>% filter (`Academic Period` != current_term)
-  }
-  
-  # Extract date from filename, supplied by MyReports
-  file_date <- str_extract(file, "[0-9]{4}[0-9]{2}[0-9]{2}")
-  
-  # Add column as_of_date for historic tracking
-  new_students$as_of_date <- ymd(file_date)
-  
   # remove noise in the xslx file 
   new_students <- new_students %>% drop_na(`Academic Year`)
   
@@ -101,42 +51,5 @@ for (file in file_list) {
   
   message("done processing academic study Excel file.") 
   
-  
-  # TODO handle different number of columns
-  # meanwhile, print out the diffs for some clue
-  if (!rebuild) {
-    message("cols in OLD not in NEW data")
-    print(setdiff(names(old_students),names(new_students)))
-    
-    message("cols in NEW not in OLD data")
-    print(setdiff(names(new_students),names(old_students)))
-    
-    # combine data if not rebuilding 
-    message("combining new data with old data...")
-    students <- rbind(old_students,new_students)
-  } 
-  else {
-    message("no old data; no need to combine anything.")
-    students <- new_students  
-  }
-  
-  message("saving feather file...") 
-  write_feather(students,paste0(cedar_data_dir,"/processed/academic-study.feather"))
-  message("saved ",nrow(students) ," rows.")
-  
-  
-  # if data archiving enabled, archive downloaded file to archive folder (from config.R)
-  if (!is.null(cedar_data_archive_dir)) {
-    message("moving .xlsx file to archive folder...")
-    
-    filepath <- as.character(file)
-    file.copy(to =   paste0(cedar_data_archive_dir,"academic-study/", basename(filepath)),
-              from = filepath)
-    file.remove(from = filepath)
-    
-    message("xlsx file archived.")
-  }
-  
-} # end process excel file
-
-message("all done in parse-academic-study.")
+return(new_students)
+}
---
+++ data-parsers/parse-data.R
@@ -0,0 +1,184 @@
+# run this script from the data-parsers folder
+pacman::p_load(optparse, tidyverse, readxl, fs, data.table, lubridate)
+
+message("Welcome to parse-data!")
+
+# load basic includes 
+source("../includes/config.R")
+source(paste0(cedar_base_dir,"/includes/mappings.R"))
+source(paste0(cedar_base_dir,"/includes/lists.R"))
+source(paste0(cedar_base_dir,"/includes/gen_ed_courses.R"))
+source(paste0(cedar_base_dir,"/includes//misc_funcs.R"))
+
+# define details for each kind of MyReports report
+report_specs <- list(
+  desr = list(
+    dir = "DESRs",
+    data_file = "DESRs",
+    term_col = "TERM",
+    parser = "parse-DESR.R"
+  ),
+  cl = list(
+    dir = "class-lists",
+    data_file = "class-list",
+    term_col = "Academic Period",
+    parser = "parse-class-list.R"
+  ),
+  as = list(
+    dir = "academic-study",
+    data_file = "academic-study",
+    term_col = "Academic Period",
+    parser = "parse-academic-study.R"
+  ),
+  deg = list(
+    dir = "degrees",
+    data_file = "degrees",
+    parser = "parse-degrees.R"
+  )
+)
+
+# define options for command line
+option_list = list(
+  make_option(c("-r","--report"), # options are "desr","cl","as","deg"
+              help="specifies what report to process."), 
+  
+  make_option(c("--guide"), default=FALSE, action="store_true",
+              help="show instructions and options for specified function."),
+  
+  make_option(c("-t", "--term"), type="character", 
+              help="term code (i.e. 202410)", metavar="character"),
+  
+  make_option(c("--onedrive"),  default=FALSE, action="store_true",
+              help="us to automatically save file to ondrive directory as specified in config.R", metavar="character")
+); 
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+
+if (is.null(opt$report)){
+  stop("No function (-r or --report) specified. Specify '-f guide' to see options. ", call.=FALSE)
+}
+
+# convert report to list
+report_list <- convert_param_to_list(opt$report)
+
+
+# loop through reports as specified in command line
+for (report in report_list) {
+  # uncomment for studio testing
+  # report <- report_list[[1]]
+  
+  message("processing report type: ", report)
+  
+  # get report specs
+  report_spec <- report_specs[[report]]
+  
+  # get list of files in downloads folder and process one by one
+  message("getting list of downloaded files...")
+  report_dir <- paste0(cedar_data_dir,'downloads/',report_spec$dir)
+  file_list <- dir_ls(report_dir)
+  
+  if (length(file_list) == 0) {
+    message("no downloaded files found in: ",report_dir)
+  }
+  
+  for (file in file_list) {
+    # uncomment for studio testing
+    # file <- file_list[1]
+    
+    message("processing file: ", file, "..." )
+    
+    # recognize if rebuilding database by detecting no file
+    # TODO: am i looking for local or cloud file or both?
+    message("loading previous data...")
+    oldfile <- paste0(cedar_data_dir,"processed/",report_spec$data_file,".Rds")
+    
+    if (file.exists(oldfile)) {
+      old_data <- readRDS(oldfile)
+      message("loaded ",nrow(old_data) ," rows.")
+      rebuild <- FALSE
+    }
+    else {
+      message("no previous data found.")
+      old_data <- tibble()
+      rebuild <- TRUE
+    }
+    
+    message("loading latest data...")
+    new_data <- read_xlsx(file)
+    message("loaded ",nrow(new_data) ," rows.")
+    
+    message("excel file loaded. processing data...")
+    
+    # remove any data from new term present in old data 
+    if (!rebuild) {
+      message("filtering out current term data in old data...")
+      new_term <- unique(new_data[[{{report_spec[["term_col"]]}}]])
+      old_data <- old_data %>% filter (!!report_spec[["term_col"]] != new_term)
+    }
+    
+    message("adding as_of_date column...")
+    
+    # Extract download date from filename, supplied by MyReports
+    file_date <- str_extract(file, "[0-9]{4}[0-9]{2}[0-9]{2}")
+    
+    # Add column as_of_date so we know how recent data is
+    new_data$as_of_date <- ymd(file_date)
+    
+    # source appropriate parser based on report type
+    message("sourcing parser...")
+    source(report_spec$parser) # defines parse function for report type
+    new_data <- parse(new_data)
+    
+    
+    # TODO handle different number of columns
+    # meanwhile, print out the diffs for some clue
+    if (!rebuild) {
+      message("cols in OLD not in NEW data")
+      print(setdiff(names(old_data),names(new_data)))
+      
+      message("cols in NEW not in OLD data")
+      print(setdiff(names(new_data),names(old_data)))
+      
+      # combine data if not rebuilding 
+      message("combining new data with old data...")
+      data <- rbind(old_data, new_data)
+    } 
+    else {
+      message("no old data; no need to combine anything.")
+      data <- new_data
+    }
+    
+    filename <- paste0(cedar_data_dir,"processed/",report_spec$data_file,".Rds")
+    message("saving Rds file: ",filename,"...")
+    saveRDS(data,file=filename)
+    message("saved ",nrow(data) ," rows.")
+
+    if (!is.null(cedar_cloud_data_dir)) {
+      message("copying Rds to local OneDrive folder...")
+      
+      file.copy(to =   cedar_cloud_data_dir,
+                from = filename)
+    }
+    
+    # message("saving feather file...") 
+    # write_feather(students,paste0(cedar_data_dir,"/processed/class-list.feather"))
+    # message("saved ",nrow(students) ," rows.")
+  
+    # if data archiving enabled, archive downloaded file to archive folder (from config.R)
+    if (!is.null(cedar_data_archive_dir)) {
+      message("moving .xlsx file to archive folder...")
+      
+      filepath <- as.character(file)
+      
+      file.copy(to =   paste0(cedar_data_archive_dir, report_spec$dir, basename(filepath)),
+                from = filepath)
+      file.remove(from = filepath)
+      
+      message("xlsx file archived.")
+    } # end if archiving data files
+  } # end process excel file
+} # end report loop
+
+message("all done in parse-data.")
