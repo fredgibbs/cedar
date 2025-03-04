45ca149fdabb2915c4b534e8f7bdf0735da5ba40
Fred Gibbs <fwgibbs@gmail.com>
2025-03-04 16:31:29 -0700

    update and simplify to work with new headcount output

--- cones/sfr.R
+++ cones/sfr.R
@@ -6,12 +6,14 @@ get_perm_faculty_count <- function() {
   load(paste0(cedar_data_dir,"processed/fac_by_term.Rda")) # loads fac_by_term DF
   
   # sum appointment percentages
-  fac_by_term_counts <- fac_by_term %>% group_by(term_code,DEPT,job_cat) %>% summarize(count = sum(`Appt %`/100))
+  fac_by_term_counts <- fac_by_term %>% group_by(term_code,DEPT,job_cat) %>% 
+    summarize(count = sum(`Appt %`/100))
   
   # only count permanent faculty in ratio calcs
   # job_cats are assigned in parse-HRreport.R
   perm_fac_count <- fac_by_term_counts %>% filter (job_cat %in% c("Professor","Lecturer","Associate Professor","Assistant Professor"))
-  perm_fac_count <- perm_fac_count %>% group_by(term_code,DEPT) %>% summarize (count = sum(count))
+  perm_fac_count <- perm_fac_count %>% group_by(term_code,DEPT) %>% 
+    summarize (count = sum(count))
   
   message("perm_fac_count:")
   perm_fac_count %>% tibble::as_tibble() %>% print(n = 20, width=Inf)
@@ -29,42 +31,57 @@ get_sfr <- function () {
   message("calling headcount to count heads in college...")  
   headcount_all <- count_heads_in_college(opt=list())
   
+  # TODO: count only majors or both majors and minors?
+  # some programs have lots of minors that should be counted 
+  # allowed_types <- c("Major")
+  # headcount_all <- headcount_all %>% filter(major_type %in% allowed_types)
+  
+  message("getting permanent faculty headcount...")  
   perm_faculty_count <- get_perm_faculty_count()
   
   message("combining student and faculty tables...")
-  studfac_ratios <- merge(headcount_all, perm_faculty_count, by.x=c("term_code","major_DEPT"), by.y=c("term_code","DEPT"),all.x=TRUE)
+  studfac_ratios <- merge(headcount_all, perm_faculty_count, by.x=c("term_code","DEPT"), by.y=c("term_code","DEPT"),all.x=TRUE)
   
   message("studfac_ratios:")
   studfac_ratios %>% tibble::as_tibble() %>% print(n = 20, width=Inf)
   
-  # filter out summer
+  # filter out summer, which is meaningless for sfr purposes
   message("filtering out summer...")
   studfac_ratios <- studfac_ratios %>% filter (!str_detect(as.character(term_code), "60"))
   
-  # remove degrees
-  # #TODO: maybe better to filter by A&S degrees to preserve that info for dept reports
+  # calc sums of majors and minors
   studfac_ratios <- studfac_ratios %>% 
-    group_by(term_code,`Academic Year`,major_DEPT,`Student Level`,count) %>% 
-    summarize(all_majors=sum(majors),all_minors=sum(minors))
+    group_by(term_code,`Academic Year`,DEPT,`Student Level`,major_name, PRGM, count)
+  majors <- studfac_ratios %>% filter (major_type %in% c("Major","Second Major")) 
+  #majors <- majors %>%  mutate(major_type = major_type, all_majors = sum(students))
+  majors <- majors %>%  summarize(major_type="all_majors", students = sum(students))
+  
+  majors %>% tibble::as_tibble() %>% print(n = 20, width=Inf)
+  
+  
+  minors <- studfac_ratios %>% filter (major_type %in% c("First Minor","Second Minor")) 
+  #minors <- minors %>% mutate(major_type = major_type, all_minors = sum(students))
+  minors <- minors %>%  summarize(major_type="all_minors", students = sum(students))
+  
+  minors %>% tibble::as_tibble() %>% print(n = 20, width=Inf)
+  
+  studfac_ratios <- rbind(majors,minors)
+  #studfac_ratios <- merge(majors,minors, by=c("term_code","DEPT","Academic Year","Student Level","major_name","PRGM","count"))
   
   message("studfac_ratios summarized:")
+  studfac_ratios <- studfac_ratios %>% group_by(term_code, DEPT,`Student Level`,major_type) %>% arrange(term_code,major_name,`Student Level`,major_type)
   studfac_ratios %>% tibble::as_tibble() %>% print(n = 20, width=Inf)
   
   # compute and add actual student faculty ratios
-  message("computing ratios...")
-  studfac_ratios$all_majors_sfr <- studfac_ratios$all_majors / studfac_ratios$count
-  studfac_ratios$all_minors_sfr <- studfac_ratios$all_minors / studfac_ratios$count
-  
   message("studfac_ratios:")
+  #studfac_ratios$all_majors_sfr <- studfac_ratios$all_majors / studfac_ratios$count
+  studfac_ratios$sfr <- studfac_ratios$students / studfac_ratios$count
   studfac_ratios %>% tibble::as_tibble() %>% print(n = 20, width=Inf)
   
-  # get average of fall and spring headcounts to calc AY ratios
-  studfac_ratios <- studfac_ratios %>% group_by(`Academic Year`,major_DEPT,`Student Level`) %>% 
-    mutate(majors_sfr_ay=mean(all_majors_sfr),minors_sfr_ay=mean(all_minors_sfr))
-  
-  message("studfac_ratios averaged over academic year:")
-  studfac_ratios %>% tibble::as_tibble() %>% print(n = 20, width=Inf)
-  
+  # get mean of fall and spring headcounts to calc AY ratios
+  # studfac_ratios <- studfac_ratios %>% group_by(`Academic Year`,DEPT,`Student Level`) %>% 
+    #mutate(sfr_ay=mean(sfr))
+
   return(studfac_ratios)
 }
 
@@ -75,55 +92,42 @@ get_sfr_data_for_dept_report <- function(d_params) {
   
   studfac_ratios <- get_sfr()
   
-  # long view for easier plotting
-  studfac_ratios_long <- studfac_ratios %>%
-    pivot_longer(
-      cols = ends_with("sfr_ay"),
-      names_to = "type",
-      values_to = "ratio",
-      values_drop_na = TRUE
-    )
-  
-  message("studfac_ratios_long:")
-  studfac_ratios_long %>% tibble::as_tibble() %>% print(n = 20, width=Inf)
-  
-
   # filter by UNDERGRADUATE and DEPT
-  ug_sfr <- studfac_ratios_long %>% 
+  ug_sfr <- studfac_ratios %>% 
     filter(`Student Level` == "Undergraduate") %>% 
-    filter(major_DEPT == d_params[["dept_code"]])
+    filter(DEPT == d_params[["dept_code"]])
   
   if (nrow(ug_sfr) > 0) {
-    ug_sfr_plot <- ggplot(ug_sfr, aes(x=`Academic Year`)) +
+    ug_sfr_plot <- ggplot(ug_sfr, aes(x=term_code)) +
       #ggtitle(paste(params["dept"], "-", params["program_str"])) +
       #ggtitle(sfr_dept_title) +
       guides(color = guide_legend(title = "")) +
       theme(legend.position="bottom") +
       labs(fill="",color="Comparison") +
       #scale_x_discrete(breaks=num.labs,labels=term.labs) +
-      geom_bar(aes(y=ratio, fill=type), stat="identity", position="dodge") +
-      xlab("Academic Year") + ylab("Students per Faculty Member")
+      geom_bar(aes(y=sfr, fill=major_type), stat="identity", position="dodge") +
+      xlab("Term") + ylab("Students per Faculty Member")
   } else {ug_sfr_plot <- "Insufficient Data"}
   
   d_params$plots[["ug_sfr_plot"]] <- ug_sfr_plot
   
   
   # filter by GRADUATE and DEPT
-  grad_sfr <- studfac_ratios_long %>% 
+  grad_sfr <- studfac_ratios %>% 
     filter(`Student Level` == "Graduate/GASM") %>% 
-    filter(major_DEPT == d_params[["dept_code"]])
+    filter(DEPT == d_params[["dept_code"]])
   
   # plot faculty ratio as grouped bars for grad and undergrad
   if (nrow(grad_sfr) > 0) {
-    grad_sfr_plot <- ggplot(grad_sfr, aes(x=`Academic Year`)) +
+    grad_sfr_plot <- ggplot(grad_sfr, aes(x=term_code)) +
       #ggtitle(paste(params["dept"], "-", params["program_str"])) +
       #ggtitle(sfr_dept_title) +
       guides(color = guide_legend(title = "")) +
       theme(legend.position="bottom") +
       #labs(fill="",color="Comparison") +
       #scale_x_discrete(breaks=num.labs,labels=term.labs) +
-      geom_bar(aes(y=ratio, fill=type), stat="identity", position="dodge") +
-      xlab("Academic Year") + ylab("Students per Faculty Member")
+      geom_bar(aes(y=sfr, fill=major_type), stat="identity", position="dodge") +
+      xlab("Term") + ylab("Students per Faculty Member")
   } else {grad_sfr_plot <- "Insufficient Data"}
   
   d_params$plots[["grad_sfr_plot"]] <- grad_sfr_plot
@@ -131,23 +135,42 @@ get_sfr_data_for_dept_report <- function(d_params) {
   
   # plot SFRs in college context
   # get sfrs for majors
-  sfr_college <- studfac_ratios_long %>%
+  sfr_college <- studfac_ratios %>%
     filter(`Student Level` == "Undergraduate") %>% 
-    filter(type == "majors_sfr_ay")
+    filter(major_type == "all_majors")
   
-  # filter to highlight dept
+  # until there is better college-level sorting, remove NAs
+  sfr_college <- sfr_college[!is.na(sfr_college$DEPT),]
+  
+  
+  message("sfr_college:")
+  sfr_college %>% tibble::as_tibble() %>% print(n = 20, width=Inf)
+  
+  # filter by DEPT code to highlight dept in college context
   sfr_college_dept <- sfr_college %>%
-    filter(major_DEPT == d_params$dept_code)
+    filter(DEPT == d_params$dept_code)
+  
+  # compress all college sfrs by dept (lose program info for simplicity)
+  sfr_college <- sfr_college %>%
+    ungroup() %>% group_by(term_code,DEPT,count) %>% 
+    summarize (all_students = sum(students), sfr=all_students/count) %>% 
+    distinct()
+  
+  message("sfr_college simplified:")
+  sfr_college %>% tibble::as_tibble() %>% print(n = 20, width=Inf)
+  
+  message("sfr_college_dept:")
+  sfr_college_dept %>% tibble::as_tibble() %>% print(n = 20, width=Inf)
   
   # scatter plot to see dept in context of college for current semester
   if (nrow(sfr_college_dept) > 0) {
-    sfr_scatterplot <- ggplot(sfr_college, aes(x=`Academic Year`, y=ratio)) +
+    sfr_scatterplot <- ggplot(sfr_college, aes(x=`term_code`, y=sfr)) +
       theme(legend.position="bottom") +
       guides(color = guide_legend(title = "",color="")) +
       geom_point(alpha=.5) +
-      geom_line(alpha=.2,aes(group=major_DEPT)) +
-      geom_point(sfr_college_dept, mapping=aes(x=`Academic Year`, y=ratio, color=major_DEPT)) +
-      geom_line(sfr_college_dept, mapping=aes(x=`Academic Year`, y=ratio, color=major_DEPT, group=major_DEPT)) +
+      geom_line(alpha=.2,aes(group=DEPT)) +
+      geom_point(sfr_college_dept, mapping=aes(x=`term_code`, y=sfr, color=major_name)) +
+      geom_line(sfr_college_dept, mapping=aes(x=`term_code`, y=sfr, color=major_name, group=major_name)) +
       xlab("Semester") + ylab("Students per Faculty")
     
     if (d_params$dept_code != "PSYC") {
