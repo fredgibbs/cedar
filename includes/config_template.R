b0ca091f2f9ecc2b2ba075508718d8daef2eaf49
Fred Gibbs <fwgibbs@gmail.com>
2025-03-19 08:56:27 -0600

    usual update

--- includes/config_template.R
+++ includes/config_template.R
@@ -12,6 +12,8 @@ cedar_data_archive_dir <- "FULL PATH TO WHERE YOU WANT TO STORE DOWNLOADED MYREP
 cedar_onedrive_dir <- "FULL PATH TO A LOCAL ONEDRIVE LOCATION"
 # example: cedar_onedrive_dir <- "~/Library/CloudStorage/OneDrive-UniversityofNewMexico/CEDAR/reports"
 
+Sys.setenv("shiny" = FALSE)
+
 # define the current term
 cedar_current_term <- 202510
 
@@ -23,15 +25,13 @@ cedar_report_end_term <- 202510
 cedar_registration_underway <- FALSE
 
 cedar_regstats_thresholds <- list()
-cedar_regstats_thresholds[["sd_buffer"]] <- 1 # % of standard deviation before flagging as concern
-cedar_regstats_thresholds[["min_count"]] <- 20 # min number of students in a course before we flag as a concern
 cedar_regstats_thresholds[["min_impacted"]] <- 20 # min difference b/w enrollment and mean (= number of students affected) 
-cedar_regstats_thresholds[["min_pct_sd"]] <- 1.2 # percent of students outside the mean compared to standard deviation
-cedar_regstats_thresholds[["min_squeeze"]] <- .5 # squeeze is ratio of avail seats to  mean attrition
+cedar_regstats_thresholds[["pct_sd"]] <- 1 # percent of students outside the mean compared to standard deviation
+cedar_regstats_thresholds[["min_squeeze"]] <- .3 # squeeze is ratio of avail seats to  mean attrition
 cedar_regstats_thresholds[["min_wait"]] <- 20 # min number of students on waitlist before being flagged
 cedar_regstats_thresholds[["section_proximity"]] <- .3 # how close to integer before rounding up/down for recommended sections? closer to .5 reduces -100s
 
 cedar_report_palette <- "Spectral"
 
 # use Rstudio's pandoc version
-rstudio_pandoc <- "/Applications/RStudio.app/Contents/MacOS/quarto/bin/tools"
+rstudio_pandoc <- "/usr/local/bin/"
