c4edcc60967e05427820af640272137dce14c437
Fred Gibbs <fwgibbs@gmail.com>
2025-05-23 16:16:44 -0600

    add new filter.R

--- data-parsers/parse-data.R
+++ data-parsers/parse-data.R
@@ -9,6 +9,7 @@ source(paste0(cedar_base_dir,"/includes/mappings.R"))
 source(paste0(cedar_base_dir,"/includes/lists.R"))
 source(paste0(cedar_base_dir,"/includes/gen_ed_courses.R"))
 source(paste0(cedar_base_dir,"/includes//misc_funcs.R"))
+source(paste0(cedar_base_dir,"/includes/filter.R"))
 
 # define details for each kind of MyReports report
 report_specs <- list(
